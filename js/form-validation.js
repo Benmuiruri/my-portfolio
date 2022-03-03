70c3358bca9d68af84f5ee238cd9ed0ebbb1077d
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-03 15:29:13 +0300

    Add email empty and lowercase form validation

---
+++ js/form-validation.js
@@ -0,0 +1,58 @@
+// Form validation
+const form = document.querySelector('#signup');
+const emailEl = document.querySelector('#user-email');
+
+// Functions to show error / success
+const showError = (input, message) => {
+  const formField = input.parentElement;
+  formField.classList.remove('sucess');
+  formField.classList.add('error');
+  const error = formField.querySelector('small');
+  error.textContent = message;
+};
+const showSuccess = (input) => {
+  const formField = input.parentElement;
+  formField.classList.remove('error');
+  formField.classList.add('success');
+  const error = formField.querySelector('small');
+  error.textContent = '';
+};
+
+// Form validation functions
+const isRequired = (value) => {
+  if (value === '') {
+    return false;
+  }
+  return true;
+};
+
+// Validate email
+const checkEmail = () => {
+  let valid = false;
+  const email = emailEl.value.trim();
+  const lower = email.toLowerCase();
+  if (!isRequired(email)) {
+    showError(emailEl, 'Email cannot be blank.');
+  } else if (lower !== email) {
+    showError(emailEl, 'Email must be lowercase');
+  } else {
+    showSuccess(emailEl);
+    valid = true;
+  }
+  return valid;
+};
+
+// Form event listener
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // validate fields
+  const isEmailValid = checkEmail();
+
+  const isFormValid = isEmailValid;
+
+  // submit to the server if the form is valid
+  if (isFormValid) {
+    form.submit();
+  }
+});
