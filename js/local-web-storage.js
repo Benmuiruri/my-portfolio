3d11ec9a6838a02c8af8920c862d6e0a197edfdc
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-04 13:51:49 +0300

    Fix js lint errors

--- js/local-web-storage.js
+++ js/local-web-storage.js
@@ -1,17 +1,18 @@
+const contactForm = document.querySelector('#signup');
 const content = JSON.parse(window.localStorage.getItem('storage'));
 function check() {
   if (localStorage.getItem('storage')) {
-    form.elements.fullName.value = content.fullName;
-    form.elements.email.value = content.email;
-    form.elements.message.value = content.message;
+    contactForm.elements.fullName.value = content.fullName;
+    contactForm.elements.email.value = content.email;
+    contactForm.elements.message.value = content.message;
   }
 }
 window.onload = check;
 
 function populateStorage() {
-  const fullName = form.elements.fullName.value;
-  const email = form.elements.email.value;
-  const message = form.elements.message.value;
+  const fullName = contactForm.elements.fullName.value;
+  const email = contactForm.elements.email.value;
+  const message = contactForm.elements.message.value;
   const storage = {
     fullName,
     email,
@@ -20,4 +21,4 @@ function populateStorage() {
   window.localStorage.setItem('storage', JSON.stringify(storage));
 }
 
-form.addEventListener('change', populateStorage);
+contactForm.addEventListener('change', populateStorage);
